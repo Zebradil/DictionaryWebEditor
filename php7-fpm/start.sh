01be998ddc5b3440d634002e431cfb09a3857578
German Lashevich <german.lashevich@gmail.com>
2017-05-15 11:40:27 +0300

    Docker compose support

---
+++ php7-fpm/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /var/www/symfony;
+
+#ln -sfn /dev/stdout var/logs/dev.log
+mkdir -p var/cache var/logs var/sessions;
+chown www-data:www-data -R var/cache var/logs var/sessions;
+
+./bin/console doctrine:database:create --if-not-exists --no-interaction --verbose
+./bin/console doctrine:schema:update --complete --force --no-interaction --verbose
+
+php-fpm;
